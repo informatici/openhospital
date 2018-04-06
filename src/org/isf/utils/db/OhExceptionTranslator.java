cec5d416083bf8d009a3a56d24f342bd03a6aa53
Roberto C <roberto@localhost>
2018-04-06 17:13:53 +0200

    Fix to intercept the exceptions launched from the Spring transactional aspect

--- src/org/isf/utils/db/OhExceptionTranslator.java
+++ src/org/isf/utils/db/OhExceptionTranslator.java
@@ -4,9 +4,12 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.isf.utils.exception.OHException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
+@Order(value=Ordered.HIGHEST_PRECEDENCE)
 @Component
 public class OhExceptionTranslator {
 
