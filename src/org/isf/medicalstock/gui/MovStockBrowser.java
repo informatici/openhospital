c1dc8f012601ec249296224717c55d3bdd8c48e7
Alessandro Domanico <alessandro.domanico@yahoo.it>
2018-08-03 12:11:07 +0200

    Upgrade to 1.8.2 - Commit 18.
    Added "units" and "packets" bundles

--- src/org/isf/medicalstock/gui/MovStockBrowser.java
+++ src/org/isf/medicalstock/gui/MovStockBrowser.java
@@ -262,7 +262,7 @@ public class MovStockBrowser extends ModalJFrame {
 		medicalPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory
 				.createLineBorder(Color.GRAY), MessageBundle.getMessage("angal.medicalstock.pharmaceutical")));
 		JPanel label1Panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		label1Panel.add(new JLabel(MessageBundle.getMessage("angal.medicalstock.description")));
+		label1Panel.add(new JLabel(MessageBundle.getMessage("angal.common.description")));
 		medicalPanel.add(label1Panel);
 		JPanel medicalDescPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		medicalDescPanel.add(getMedicalBox());
--- src/org/isf/medicalstock/gui/MovStockMultipleDischarging.java
+++ src/org/isf/medicalstock/gui/MovStockMultipleDischarging.java
@@ -83,7 +83,7 @@ public class MovStockMultipleDischarging extends JDialog {
 	private JDateChooser jDateChooser;
 	private JComboBox jComboBoxDestination;
 	private JTable jTableMovements;
-	private final String[] columnNames = { MessageBundle.getMessage("angal.medicalstock.multipledischarging.code"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.description"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.unitpack"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.qty"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.unitpack"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.total"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.lotnumberabb"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.expiringdate")}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$
+	private final String[] columnNames = { MessageBundle.getMessage("angal.common.codem"), MessageBundle.getMessage("angal.common.descriptionm"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.unitpack"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.qty"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.unitpack"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.total"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.lotnumberabb"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.expiringdate")}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$
 	private final Class[] columnClasses = { String.class, String.class, Integer.class, Integer.class, String.class, Integer.class, String.class, String.class};
 	private boolean[] columnEditable = { false, false, false, false, true, false, false, false};
 	private int[] columnWidth = { 50, 100, 70, 50, 70, 50, 100, 80};
@@ -95,7 +95,7 @@ public class MovStockMultipleDischarging extends JDialog {
 	private HashMap<String, Medical> medicalMap;
 	private ArrayList<Integer> units;
 	private JTableModel model;
-	private String[] qtyOption = new String[] { MessageBundle.getMessage("angal.medicalstock.multipledischarging.units"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.packet") }; //$NON-NLS-1$ //$NON-NLS-2$
+	private String[] qtyOption = new String[] { MessageBundle.getMessage("angal.medicalstock.multipledischarging.units"), MessageBundle.getMessage("angal.medicalstock.multipledischarging.packets") }; //$NON-NLS-1$ //$NON-NLS-2$
 	private final int UNITS = 0;
 	private final int PACKETS = 1;
 	private int optionSelected = UNITS;
