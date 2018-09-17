3541b6be7fea4cb029c510cc38f88a4f2e90430b
Alessandro Domanico <alessandro.domanico@yahoo.it>
2018-09-17 12:18:55 +0200

    Module 'lab': fixed date comparison in queries for gui and prints

--- src/org/isf/lab/service/LabIoOperationRepository.java
+++ src/org/isf/lab/service/LabIoOperationRepository.java
@@ -12,12 +12,12 @@ import org.springframework.data.repository.query.Param;
 public interface LabIoOperationRepository extends JpaRepository<Laboratory, Integer> {
 
     @Query(value = "SELECT * FROM LABORATORY JOIN EXAM ON LAB_EXA_ID_A = EXA_ID_A "
-    		+ "WHERE LAB_EXAM_DATE >= :dateFrom AND LAB_EXAM_DATE <= :dateTo ORDER BY LAB_EXAM_DATE", nativeQuery= true)
+    		+ "WHERE LAB_EXAM_DATE >= DATE(:dateFrom) AND LAB_EXAM_DATE <= DATE(:dateTo) ORDER BY LAB_EXAM_DATE", nativeQuery= true)
     public List<Laboratory> findAllWhereDatesByOrderExamDate(
     		@Param("dateFrom") GregorianCalendar dateFrom,
     		@Param("dateTo") GregorianCalendar dateTo);
     @Query(value = "SELECT * FROM LABORATORY JOIN EXAM ON LAB_EXA_ID_A = EXA_ID_A "
-    		+ "WHERE LAB_EXAM_DATE >= :dateFrom AND LAB_EXAM_DATE <= :dateTo AND EXA_DESC = :exam ORDER BY LAB_EXAM_DATE", nativeQuery= true)
+    		+ "WHERE LAB_EXAM_DATE >= DATE(:dateFrom) AND LAB_EXAM_DATE <= DATE(:dateTo) AND EXA_DESC = :exam ORDER BY LAB_EXAM_DATE", nativeQuery= true)
     public List<Laboratory> findAllWhereDatesAndExamByOrderExamDate(
     		@Param("dateFrom") GregorianCalendar dateFrom,
     		@Param("dateTo") GregorianCalendar dateTo,
@@ -32,13 +32,13 @@ public interface LabIoOperationRepository extends JpaRepository<Laboratory, Inte
     
     @Query(value = "SELECT * FROM (LABORATORY JOIN EXAM ON LAB_EXA_ID_A = EXA_ID_A)" +
 		       " JOIN EXAMTYPE ON EXC_ID_A = EXA_EXC_ID_A" +
-			   " WHERE LAB_DATE >= :dateFrom AND LAB_DATE <= :dateTo ORDER BY EXC_DESC", nativeQuery= true)
+			   " WHERE LAB_EXAM_DATE >= DATE(:dateFrom) AND LAB_EXAM_DATE <= DATE(:dateTo) ORDER BY EXC_DESC", nativeQuery= true)
     public List<Laboratory> findAllWhereDatesForPrint(
     		@Param("dateFrom") GregorianCalendar dateFrom,
-    		@Param("dateFrom") GregorianCalendar dateTo);
+    		@Param("dateTo") GregorianCalendar dateTo);
     @Query(value = "SELECT * FROM (LABORATORY JOIN EXAM ON LAB_EXA_ID_A = EXA_ID_A)" +
 		       " JOIN EXAMTYPE ON EXC_ID_A = EXA_EXC_ID_A" +
-			   " WHERE LAB_DATE >= :dateFrom AND LAB_DATE <= :dateTo AND EXA_DESC LIKE %:exam% ORDER BY EXC_DESC", nativeQuery= true)
+			   " WHERE LAB_EXAM_DATE >= DATE(:dateFrom) AND LAB_EXAM_DATE <= DATE(:dateTo) AND EXA_DESC LIKE %:exam% ORDER BY EXC_DESC", nativeQuery= true)
     public List<Laboratory> findAllWhereDatesAndExamForPrint(
     		@Param("dateFrom") GregorianCalendar dateFrom,
     		@Param("dateTo") GregorianCalendar dateTo,
