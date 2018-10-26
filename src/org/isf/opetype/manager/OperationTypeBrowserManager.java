9c08a15b890eeb7c6791629e7c54c529bff6287d
alekseykashytsa <aleksey.kashytsa@intesys.it>
2018-10-26 17:34:11 +0200

    OP-38 #comment moved gui to manager #time 0.5h

--- src/org/isf/opetype/manager/OperationTypeBrowserManager.java
+++ src/org/isf/opetype/manager/OperationTypeBrowserManager.java
@@ -1,11 +1,16 @@
 package org.isf.opetype.manager;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.isf.generaldata.MessageBundle;
+import org.isf.medtype.model.MedicalType;
 import org.isf.menu.gui.Menu;
 import org.isf.opetype.model.OperationType;
 import org.isf.opetype.service.OperationTypeIoOperation;
 import org.isf.utils.exception.OHServiceException;
+import org.isf.utils.exception.model.OHExceptionMessage;
+import org.isf.utils.exception.model.OHSeverityLevel;
 
 public class OperationTypeBrowserManager {
 
@@ -29,6 +34,15 @@ public class OperationTypeBrowserManager {
 	 * @throws OHServiceException 
 	 */
 	public boolean newOperationType(OperationType operationType) throws OHServiceException {
+        List<OHExceptionMessage> errors = validateOperationType(operationType);
+        if(!errors.isEmpty()){
+            throw new OHServiceException(errors);
+        }
+        if (codeControl(operationType.getCode())){
+            throw new OHServiceException(new OHExceptionMessage(null,
+                    MessageBundle.getMessage("angal.common.codealreadyinuse"),
+                    OHSeverityLevel.ERROR));
+        }
         return ioOperations.newOperationType(operationType);
 	}
 
@@ -40,6 +54,10 @@ public class OperationTypeBrowserManager {
 	 * @throws OHServiceException 
 	 */
 	public boolean updateOperationType(OperationType operationType) throws OHServiceException {
+        List<OHExceptionMessage> errors = validateOperationType(operationType);
+        if(!errors.isEmpty()){
+            throw new OHServiceException(errors);
+        }
         return ioOperations.updateOperationType(operationType);
 	}
 	
@@ -63,4 +81,26 @@ public class OperationTypeBrowserManager {
 	public boolean codeControl(String code) throws OHServiceException {
         return ioOperations.isCodePresent(code);
 	}
+
+    protected List<OHExceptionMessage> validateOperationType(OperationType operationType) {
+        String key = operationType.getCode();
+        String description = operationType.getDescription();
+        List<OHExceptionMessage> errors = new ArrayList<OHExceptionMessage>();
+        if(key.isEmpty() ){
+            errors.add(new OHExceptionMessage("codeEmptyError",
+                    MessageBundle.getMessage("angal.opetype.pleaseinsertacode"),
+                    OHSeverityLevel.ERROR));
+        }
+        if(key.length()>1){
+            errors.add(new OHExceptionMessage("codeTooLongError",
+                    MessageBundle.getMessage("angal.opetype.codetoolongmaxchars"),
+                    OHSeverityLevel.ERROR));
+        }
+        if(description.isEmpty() ){
+            errors.add(new OHExceptionMessage("descriptionEmptyError",
+                    MessageBundle.getMessage("angal.opetype.pleaseinsertavaliddescription"),
+                    OHSeverityLevel.ERROR));
+        }
+        return errors;
+    }
 }
