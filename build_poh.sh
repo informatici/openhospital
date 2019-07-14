9d3a2edbf4e96a96913667ab7bbbc035eaf299a3
Paolo <pviotti@users.noreply.github.com>
2019-07-14 11:18:21 +0100

    add support for creating linux poh distribution

--- build_poh.sh
+++ build_poh.sh
@@ -2,21 +2,28 @@
 
 set -e
 
-if [ ! -d ./poh-bundle-win/poh ]; then
-    mkdir ./poh-bundle-win/poh
-fi
-cp -r ./gui/target/OpenHospital20/* ./poh-bundle-win/poh
+# set the portable distribution version
+poh_version="0.0.5"
 
-# get the version
+# get the Open Hospital version
+test -f ./gui/rsc/version.properties
 version_file="./gui/rsc/version.properties"
 major=$(grep VER_MAJOR $version_file | cut -d"=" -f2)
 minor=$(grep VER_MINOR $version_file | cut -d"=" -f2)
 release=$(grep VER_RELEASE $version_file | cut -d"=" -f2)
 version="$major.$minor.$release"
 
-poh_version="0.0.5"
+mkdir -p ./poh-bundle-win/oh
+mkdir -p ./poh-bundle-linux/oh
+cp -rf ./gui/target/OpenHospital20/* ./poh-bundle-win/oh
+cp -rf ./gui/target/OpenHospital20/* ./poh-bundle-linux/oh
 
 cd ./poh-bundle-win
-zip -r poh-win32-$poh_version-$version.zip *
+zip -r ../poh-win32-$poh_version-$version.zip *
 cd ..
 
+cd ./poh-bundle-linux
+zip -r ../poh-linux-$poh_version-$version.zip *
+cd ..
+
+echo "Portable distributions of Open Hospital created successfully."
