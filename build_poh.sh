8ffd11680d8c6ff36ddbc78409af71e1a9fe9cfd
Alessandro Domanico <alessandro.domanico@yahoo.it>
2019-07-26 23:52:36 +0200

    Update build_poh.sh

--- build_poh.sh
+++ build_poh.sh
@@ -18,11 +18,11 @@ version="$major.$minor.$release"
 
 mkdir -p ./poh-bundle-win/oh
 cp -rf ./gui/target/OpenHospital20/* ./poh-bundle-win/oh
-cp *.sql ./poh-bundle-win/oh
+cp *.sql ./poh-bundle-win
 
 mkdir -p ./poh-bundle-linux/oh
 cp -rf ./gui/target/OpenHospital20/* ./poh-bundle-linux/oh
-cp *.sql ./poh-bundle-linux/oh
+cp *.sql ./poh-bundle-linux
 
 cd ./poh-bundle-win
 zip -r ../poh-win32-$poh_version-core-$version.zip *
