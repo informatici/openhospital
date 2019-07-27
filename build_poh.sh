b1d7db4144189aef116552b1fae97bd0068ced4d
Alessandro Domanico <alessandro.domanico@yahoo.it>
2019-07-27 23:06:48 +0300

    Issue 10 (#11)
    Updated build_poh.sh
    - Include root folder in the releases' archives 
    - differentiate poh versions
    - adding comments and checks

--- build_poh.sh
+++ build_poh.sh
@@ -4,7 +4,8 @@
 set -e
 
 # set the portable distribution version
-poh_version="0.0.5"
+poh_win32_version="0.0.5"
+poh_linux_version="0.0.3"
 
 # get the Open Hospital version
 test -f ./gui/rsc/version.properties
@@ -14,23 +15,23 @@ minor=$(grep VER_MINOR $version_file | cut -d"=" -f2)
 release=$(grep VER_RELEASE $version_file | cut -d"=" -f2)
 version="$major.$minor.$release"
 
-mkdir -p ./poh-bundle-win/oh
-cp -rf ./gui/target/OpenHospital20/* ./poh-bundle-win/oh
-cp *.sql ./poh-bundle-win
+# assembling windows portable
+mkdir -p ./poh-win32-$poh_win32_version-core-$version/oh
+cp -rf ./poh-bundle-win/* ./poh-win32-$poh_win32_version-core-$version
+cp -rf ./gui/target/OpenHospital20/* ./poh-win32-$poh_win32_version-core-$version/oh
+cp *.sql ./poh-win32-$poh_win32_version-core-$version
 
-mkdir -p ./poh-bundle-linux/oh
-cp -rf ./gui/target/OpenHospital20/* ./poh-bundle-linux/oh
-cp *.sql ./poh-bundle-linux
+# assembling linux portable 
+mkdir -p ./poh-linux-$poh_linux_version-core-$version/oh
+cp -rf ./poh-bundle-linux/* ./poh-linux-$poh_linux_version-core-$version
+cp -rf ./gui/target/OpenHospital20/* ./poh-linux-$poh_linux_version-core-$version/oh
+cp *.sql ./poh-linux-$poh_linux_version-core-$version
 
-cd ./poh-bundle-win
-zip -r ../poh-win32-$poh_version-core-$version.zip *
-cd ..
+# packaging
+zip -r poh-win32-$poh_win32_version-core-$version.zip poh-win32-$poh_win32_version-core-$version
+tar -cvzf poh-linux-$poh_linux_version-core-$version.tar.gz poh-linux-$poh_linux_version-core-$version
 
-cd ./poh-bundle-linux
-tar -cvzf ../poh-linux-$poh_version-core-$version.tar.gz *
-cd ..
-
-
-cp -rf ./gui/target/OpenHospital20/* ./poh-bundle-win/oh
+# check
+ls
 
 echo "Portable distributions of Open Hospital created successfully."
