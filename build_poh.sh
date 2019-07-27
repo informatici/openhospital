c13bfeb1476b2da41cfa97747bef236a1cfecf46
Alessandro Domanico <alessandro.domanico@yahoo.it>
2019-07-27 21:57:13 +0200

    Update build_poh.sh
    differentiate poh versions

--- build_poh.sh
+++ build_poh.sh
@@ -4,7 +4,8 @@
 set -e
 
 # set the portable distribution version
-poh_version="0.0.5"
+poh_win32_version="0.0.5"
+poh_linux_version="0.0.3"
 
 # get the Open Hospital version
 test -f ./gui/rsc/version.properties
@@ -14,18 +15,18 @@ minor=$(grep VER_MINOR $version_file | cut -d"=" -f2)
 release=$(grep VER_RELEASE $version_file | cut -d"=" -f2)
 version="$major.$minor.$release"
 
-mkdir -p ./poh-win32-$poh_version-core-$version/oh
-cp -rf ./poh-bundle-win/* ./poh-win32-$poh_version-core-$version
-cp -rf ./gui/target/OpenHospital20/* ./poh-win32-$poh_version-core-$version/oh
-cp *.sql ./poh-win32-$poh_version-core-$version
+mkdir -p ./poh-win32-$poh_win32_version-core-$version/oh
+cp -rf ./poh-bundle-win/* ./poh-win32-$poh_win32_version-core-$version
+cp -rf ./gui/target/OpenHospital20/* ./poh-win32-$poh_win32_version-core-$version/oh
+cp *.sql ./poh-win32-$poh_win32_version-core-$version
 
-mkdir -p ./poh-linux-$poh_version-core-$version/oh
-cp -rf ./poh-bundle-linux/* ./poh-linux-$poh_version-core-$version
-cp -rf ./gui/target/OpenHospital20/* ./poh-linux-$poh_version-core-$version/oh
-cp *.sql ./poh-linux-$poh_version-core-$version
+mkdir -p ./poh-linux-$poh_linux_version-core-$version/oh
+cp -rf ./poh-bundle-linux/* ./poh-linux-$poh_linux_version-core-$version
+cp -rf ./gui/target/OpenHospital20/* ./poh-linux-$poh_linux_version-core-$version/oh
+cp *.sql ./poh-linux-$poh_linux_version-core-$version
 
-zip -r poh-win32-$poh_version-core-$version.zip poh-win32-$poh_version-core-$version
+zip -r poh-win32-$poh_win32_version-core-$version.zip poh-win32-$poh_win32_version-core-$version
 
-tar -cvzf poh-linux-$poh_version-core-$version.tar.gz poh-linux-$poh_version-core-$version
+tar -cvzf poh-linux-$poh_linux_version-core-$version.tar.gz poh-linux-$poh_linux_version-core-$version
 
 echo "Portable distributions of Open Hospital created successfully."
