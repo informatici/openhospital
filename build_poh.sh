f325681f5e8f3e43974a4f255cf465578c3a28ba
Paolo <pviotti@users.noreply.github.com>
2019-07-28 21:28:06 +0100

    move build step and database dump from travis to bash script

--- build_poh.sh
+++ build_poh.sh
@@ -15,15 +15,22 @@ minor=$(grep VER_MINOR $version_file | cut -d"=" -f2)
 release=$(grep VER_RELEASE $version_file | cut -d"=" -f2)
 version="$major.$minor.$release"
 
-# converting documentation
+# compile core and gui projects
+docker-compose -f core/docker-compose.yml up -d
+mvn package
+
+# dump the database to a SQL script
+mysqldump --protocol tcp -h localhost -u isf -pisf123 --compatible=mysql40 oh > database.sql
+
+# convert documentation
 asciidoctor-pdf ./doc/doc_admin/AdminManual.adoc -o AdminManual.pdf
 asciidoctor-pdf ./doc/doc_user/UserManual.adoc -o UserManual.pdf
 
-# estracting changelogs
+# extract changelogs
 cp core/doc/`ls core/doc/ -r | head -n 1` core_`ls core/doc/ -r | head -n 1`
 cp gui/doc/`ls gui/doc/ -r | head -n 1` gui_`ls gui/doc/ -r | head -n 1`
 
-# assembling windows portable
+# assemble OH Windows portable
 mkdir -p ./poh-win32-$poh_win32_version-core-$version/oh/doc
 cp -rf ./poh-bundle-win/* ./poh-win32-$poh_win32_version-core-$version
 cp -rf ./gui/target/OpenHospital20/* ./poh-win32-$poh_win32_version-core-$version/oh
@@ -31,7 +38,7 @@ cp *.sql ./poh-win32-$poh_win32_version-core-$version
 cp *.pdf ./poh-win32-$poh_win32_version-core-$version/oh/doc
 cp *.txt ./poh-win32-$poh_win32_version-core-$version/oh/doc
 
-# assembling linux portable 
+# assemble OH Linux portable
 mkdir -p ./poh-linux-$poh_linux_version-core-$version/oh/doc
 cp -rf ./poh-bundle-linux/* ./poh-linux-$poh_linux_version-core-$version
 cp -rf ./gui/target/OpenHospital20/* ./poh-linux-$poh_linux_version-core-$version/oh
@@ -39,11 +46,14 @@ cp *.sql ./poh-linux-$poh_linux_version-core-$version
 cp *.pdf ./poh-linux-$poh_linux_version-core-$version/oh/doc
 cp *.txt ./poh-linux-$poh_linux_version-core-$version/oh/doc
 
-# packaging
+# package
 zip -r poh-win32-$poh_win32_version-core-$version.zip poh-win32-$poh_win32_version-core-$version
 tar -cvzf poh-linux-$poh_linux_version-core-$version.tar.gz poh-linux-$poh_linux_version-core-$version
 
 # check
 ls
+
+# cleanup
+docker-compose -f core/docker-compose.yml down
 
 echo "Portable distributions of Open Hospital created successfully."
