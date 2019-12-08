ed13338ea75cd1f1edb50600128d899b171ac477
Alessandro Domanico <alessandro.domanico@yahoo.it>
2019-12-08 02:12:13 +0100

    OP-109 #comment converting package AgeType
    and removed all warnings and unwanted TODOs

--- src/org/isf/agetype/service/AgeTypeIoOperationRepository.java
+++ src/org/isf/agetype/service/AgeTypeIoOperationRepository.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 import org.isf.agetype.model.AgeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface AgeTypeIoOperationRepository extends JpaRepository<AgeType, String> {
     public List<AgeType> findAllByOrderByCodeAsc();	
     public AgeType findOneByCode(String code);
